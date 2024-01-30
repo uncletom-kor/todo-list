244be97229848d51cbdf7fc34a589049d8319cd0
github uncletom-kor <gituhb uncletom.kor@gmail.com>
2024-01-30 10:14:14 +0900

    App.js File Modified at 20240130 1013
    App.js와 App.jsx가 같이 존재해서 App.jsx를 삭제한 후 App.js를 App.jsx로 rename함.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-// 필요한 모듈과 구성 요소를 가져온다.
-import './App.css'; // App.css 파일에서 스타일을 가져온다.
-import Button1 from './components/Button1'; // Button1 컴포넌트를 가져온다.
-import Button2 from './components/Button2'; // Button2 컴포넌트를 가져온다.
-import StyleComponents from './components/Style-component';
-import TailwindComponent from './components/Tailwind-component';
+import './App.css';
+import TodoList from './components/TodoList/TodoList';
 
 export default function App() {
     return (
-        <>
-            <Button1 />
-            <Button2 />
-            <StyleComponents />
-            <TailwindComponent />
-        </>
+        <div>
+            <TodoList />
+        </div>
     );
 }
